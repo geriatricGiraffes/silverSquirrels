d3a7ba0649fbc1a0a048a4f586a1fe2719b04869
Aimee Rosato <aimee.rosato@gmail.com>
2016-03-05 16:57:51 -0500

    (bugfix) changed limit on number of trials posted from server

--- server/server.js
+++ server/server.js
@@ -55,10 +55,9 @@ app.post('/api/coords', function(req, res){
   var radius = req.body.radius;
   var lat = req.body.lat;
   var long = req.body.long;
-  var limit = 30;
 
 // Unirest is used to get API data, following example on trailAPI website
-  unirest.get("https://trailapi-trailapi.p.mashape.com/?lat="+lat+"&"+limit+"=20&lon="+long+"&q[activities_activity_type_name_eq]=hiking&radius="+radius)
+  unirest.get("https://trailapi-trailapi.p.mashape.com/?lat="+lat+"&limit=20&lon="+long+"&q[activities_activity_type_name_eq]=hiking&radius="+radius)
     .header("X-Mashape-Key", process.env.TRAIL_API_KEY)
     .header("Accept", "text/plain")
   .end(function(result){
